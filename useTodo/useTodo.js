d9e240abe82599daf25aa62d41b754bb1968b6e5
Javi <j.morcillo@ayladt.com>
2023-05-18 13:43:29 +0200

    todoReducer agregado

--- useTodo/useTodo.js
+++ useTodo/useTodo.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer, useState } from "react"
-import { todoReducer } from "../08-useReducer/todoReducer"
+import { todoReducer } from "./todoReducer"
 
 const init = () => 
 {
